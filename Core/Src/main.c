4174a5601c5ae9df93c851753c120cf713fcb70f
Ming-der Wang <mingderwang@gmail.com>
2022-12-26 16:46:38 +0800

    remove GPIO_Init

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -90,7 +90,6 @@ const osEventFlagsAttr_t myEvent01_attributes = {
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 void StartDefaultTask(void *argument);
 void StartTask02(void *argument);
 void Callback01(void *argument);
@@ -131,7 +130,6 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -244,19 +242,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
 }
 
 /* USER CODE BEGIN 4 */
